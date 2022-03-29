a4831a17c0df187175f63a428f31877d1621ee1f
Faluis2003 <98657236+Faluis2003@users.noreply.github.com>
2022-03-29 22:15:21 +0200

    Fonction estpair et main ont été ajoutées

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -11,7 +11,32 @@
 
 //Fonctions réalisées par l'étudiant 2 :
 
+#include <iostream>
+using namespace std;
 
+/* Fonctions*/
+bool estpair(int x){      /* Fonction pour voir si un nombre est pair ou pas */
+    if(x % 2 == 0){     /* On regarde si le resultat de la division euclidienne = a 0 */
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int sommeNombre(int x){
+}
+
+int main(){
+    int n;
+ 
+    cout << "Entrer un nombre :";   /* Demander un int */
+    cin >> n;   /* Stocker la la valeur dans n */
+    
+    cout << estpair(n); /* Afficher le resultat 0=impair, 1=pair*/
+    
+    return 0;
+}
 
 
 

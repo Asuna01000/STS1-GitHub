8867a4af1472ef05e1871bde6e876a38247926b2
Asuna01000 <101389128+Asuna01000@users.noreply.github.com>
2022-03-30 13:47:01 +0200

    Ajout de la fonction estMajeur
    J'ajoute une fonction (estMajeur)

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,12 +1,18 @@
 //Fonctions réalisées par l'étudiant 1 :
 
 
+bool estMajeur(int age)
+    {
+        if (age >= 18)
+            {
+                return true ;
+            }
 
-
-
-
-
-
+        else 
+            {
+                return false  ;
+            }  
+    }
 
 
 //Fonctions réalisées par l'étudiant 2 :
